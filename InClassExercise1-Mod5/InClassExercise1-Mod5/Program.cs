8954af0dce635da8390d991d387b3a056037b256
Holden Halferty <holden.halferty@okstate.edu>
2020-02-24 11:18:11 -0600

    Can take user selection of product, and take their payment. Still have an error calculating the current balance and refunding.

--- InClassExercise1-Mod5/InClassExercise1-Mod5/Program.cs
+++ InClassExercise1-Mod5/InClassExercise1-Mod5/Program.cs
@@ -12,40 +12,79 @@ namespace InClassExercise1_Mod5
     {
         static void Main(string[] args)
         {
-            int userSelection;
-            double itemPrice;
 
-            Console.WriteLine("WELCOME TO THE VENDING MACHINE");
             while (true)
             {
+                int userSelection;
+                string product = "";
+                double itemPrice = 0;
+                double totalPayment = 0;
+                double currBalance = 0;
 
-                /* Take user input for the vending machine selection. */
-                Console.Write("We can offer you (1)soda - $1, (2)cookies - $2, or (3)chips - $1.50.\nPlease select a product(Enter to exit): ");
-                try
+                bool takeProduct = true;
+                Console.WriteLine("WELCOME TO THE VENDING MACHINE");
+                while (takeProduct)
                 {
-                    userSelection = Convert.ToInt32(Console.ReadLine());
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Not a valid selection. Please try again.");
-                    continue;
-                }
 
-                /* Handle setting itemPrice given the user input.  */
-                switch (userSelection)
-                {
-                    case 1:
-                        itemPrice = 1.00;
-                        break;
-                    case 2:
-                        itemPrice = 2.00;
-                        break;
-                    case 3:
-                        itemPrice = 1.50;
-                        break;
+                    /* Take user input for the vending machine selection. */
+                    Console.Write("We can offer you (1)soda - $1, (2)cookies - $2, or (3)chips - $1.50.\nPlease select a product: ");
+                    try
+                    {
+                        userSelection = Convert.ToInt32(Console.ReadLine());
+                        takeProduct = false;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Not a valid selection. Please try again.");
+                        continue;
+                    }
 
-                }
+                    /* Handle setting itemPrice given the user input.  */
+                    switch (userSelection)
+                    {
+                        case 1:
+                            itemPrice = 1.00;
+                            product = "soda";
+                            break;
+                        case 2:
+                            itemPrice = 2.00;
+                            product = "cookies";
+                            break;
+                        case 3:
+                            itemPrice = 1.50;
+                            product = "chips";
+                            break;
 
+                    }
+
+                    /* take user payment. */
+                    currBalance = itemPrice - totalPayment;
+                    while (totalPayment < currBalance)
+                    {
+                        double payment = 0;
+                        currBalance = itemPrice - totalPayment;
+                        Console.WriteLine("You still owe {0} for your {1}.", currBalance, product);
+                        Console.Write("Please insert a coin : ");
+                        try
+                        {
+                            payment = Convert.ToInt32(Console.ReadLine());
+                            if (payment != 5 && payment != 10 && payment != 25)
+                            {
+                                Console.WriteLine("Not a valid input. Please try again.");
+                                continue;
+                            }
+                            /* If payment is accepted, add to the total payment. */
+                            payment = payment / 100;
+                            totalPayment = totalPayment + payment;
+                            Console.WriteLine(totalPayment);
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Not a valid input. Please try again.") ;
+                            continue;
+                        }
+                    }
+                } 
             }
         }
     }
