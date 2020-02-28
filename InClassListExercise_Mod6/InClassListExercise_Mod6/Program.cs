a698c850c8a90386674b1dbcaeaeb14eb75e13fc
Holden Halferty <holden.halferty@okstate.edu>
2020-02-28 11:09:17 -0600

    Exercise with lists. Messed around with a recursive function. has a bug. Index out of bounds.

---
+++ InClassListExercise_Mod6/InClassListExercise_Mod6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InClassListExercise_Mod6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myNums = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("Original List");
+            for (int i = 0; i < myNums.Count; i++)
+            {
+                Console.WriteLine("Element #{0} : {1}", i, myNums[i] );
+            }
+
+
+            List<int> emptyList = new List<int>();
+            List<int> revList = revRecur(myNums, emptyList);
+           
+            Console.WriteLine("\nReverseList");
+            for (int i = 0; i < myNums.Count; i++)
+            {
+                Console.Write(revList[i] + " ");
+            }
+            Console.ReadKey();
+        }
+
+        /* recursive function that takes an input list and returns a list that is reversed.  */
+        public static List<int> revRecur (List<int> inList, List<int> outList)
+        {
+            /* Base Case */
+            if (inList.Count == outList.Count)
+            {
+                Console.WriteLine("Done reversing.");
+            }
+            /* Inductive Case */
+            else
+            { 
+                outList.Add(inList.Count - 1);
+                inList.RemoveAt(inList.Count - 1);
+                revRecur(inList, outList);
+            }
+            return outList;
+        }
+    }
+}
