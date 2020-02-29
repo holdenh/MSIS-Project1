1c2e1afc095c59da81409dd31870b0b9b037a058
holdenh <holden.halferty@okstate.edu>
2020-02-29 16:30:37 -0600

    Project is complete.

--- Project2/Project2/Project2/Program.cs
+++ Project2/Project2/Project2/Program.cs
@@ -13,7 +13,6 @@ namespace Project2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
             /* Variables */
             Random rand = new Random();
@@ -23,6 +22,7 @@ namespace Project2
             List<string> suits = new List<string>() { "Spades", "Hearts", "Diamods", "Clubs" };
             List<string> ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
             List<string> pickedSuits = new List<string>();
+            List<int> pickedCards = new List<int>();
             string cardSuit;
             string cardValue;
             int card;
@@ -31,6 +31,7 @@ namespace Project2
             int drawCount = 0;
 
             /* loop while we have not selected atleast one card from each of the four suits. */
+            Console.WriteLine("Cards drawn :");
             while (remainingSuits != 0)
             {
                 randIndex = rand.Next(0, deck.Count);
@@ -38,18 +39,26 @@ namespace Project2
                 cardSuit = suits[(card / 13)];
                 cardValue = ranks[(card % 13)];
 
-                Console.WriteLine("\tCard #{0}: is the {1} of {2}.", card, cardValue, cardSuit);
+                Console.WriteLine("\tCard #{0}   : is the {1} of {2}.", card, cardValue, cardSuit);
 
                 /* check if the suit has already had a card selected. */
                 if (!pickedSuits.Contains(cardSuit))
                 {
                     /* For a new suit, add to the list and decrement the counter. */
                     pickedSuits.Add(cardSuit);
+                    pickedCards.Add(card);
                     remainingSuits--;
                 }
                 drawCount++;
             }
-            Console.WriteLine("\nIt took {0} draws to have atleast one card from each suit to be drawn.", drawCount);
+            Console.WriteLine("\nCards that make Suit Collection :");
+            foreach (int pCard in pickedCards)
+            {
+                cardSuit = suits[(pCard / 13)];
+                cardValue = ranks[(pCard % 13)];
+                Console.WriteLine("\tCard #{0}  : is the {1} of {2}.", pCard, cardValue, cardSuit);
+            }
+            Console.WriteLine("\nNumber of draws: {0} ", drawCount);
             Console.ReadKey();
         }
     }
