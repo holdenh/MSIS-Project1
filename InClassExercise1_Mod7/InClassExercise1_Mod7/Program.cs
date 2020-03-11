5498ae37bab866d18528fc5058292d41c0002b3c
Holden Halferty <holden.halferty@okstate.edu>
2020-03-11 10:35:57 -0500

    Added Exam1 gitignore.

---
+++ InClassExercise1_Mod7/InClassExercise1_Mod7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InClassExercise1_Mod7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
