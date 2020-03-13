22f5839aa18fbcd2ad623f298088adad8b6122e3
Holden Halferty <holden.halferty@okstate.edu>
2020-03-13 11:13:20 -0500

    changed values to better represent actual prices. Still have a bug with calculating the refund on the reciept.

--- InClassExercise1_Mod7/InClassExercise1_Mod7/Program.cs
+++ InClassExercise1_Mod7/InClassExercise1_Mod7/Program.cs
@@ -9,23 +9,23 @@ namespace InClassExercise1_Mod7
         {
 
             bool exit = false;
-            Dictionary<string, List<double>> receipt = new Dictionary<string, List<double>>(); // product, {quantity, unitPrice}
+            Dictionary<string, List<decimal>> receipt = new Dictionary<string, List<decimal>>(); // product, {quantity, unitPrice}
             int userSelection;
             string product = "";
             int amount = 0;
-            double itemPrice = 0;
-            double totalPayment = 0;
-            double currBalance = 0;
-            double refund = 0;
+            decimal itemPrice = 0.0M;
+            decimal totalPayment = 0.0M;
+            decimal currBalance = 0.0M;
+            decimal refund = 0.0M;
             while (!exit)
             {
                /* reset all variables. */
                 product = "";
                 amount = 0;
-                itemPrice = 0;
-                totalPayment = 0;
-                currBalance = 0;
-                refund = 0;
+                itemPrice = 0.0M;
+                totalPayment = 0.0M;
+                currBalance = 0.0M;
+                refund = 0.0M;
 
                 bool takeProduct = true;
                 Console.WriteLine("WELCOME TO THE VENDING MACHINE\n\n");
@@ -52,15 +52,15 @@ namespace InClassExercise1_Mod7
                     switch (userSelection)
                     {
                         case 1:
-                            itemPrice = 100;
+                            itemPrice = 1.00M;
                             product = "soda";
                             break;
                         case 2:
-                            itemPrice = 200;
+                            itemPrice = 2.00M;
                             product = "cookies";
                             break;
                         case 3:
-                            itemPrice = 150;
+                            itemPrice = 1.50M;
                             product = "chips";
                             break;
                         default:
@@ -74,7 +74,7 @@ namespace InClassExercise1_Mod7
                     /* Add the product to the dictionary. use produce amount. */
                     if (!receipt.ContainsKey(product))
                     {
-                        List<double> productDetails = new List<double>() { amount, itemPrice};
+                        List<decimal> productDetails = new List<decimal>() { amount, itemPrice};
                         receipt[product] = productDetails;
 
                     }
@@ -84,8 +84,8 @@ namespace InClassExercise1_Mod7
                     }
                     while (currBalance > 0)
                     {
-                        Console.WriteLine("\nYou owe {0} cents for your {1}.", currBalance, product);
-                        int payment = 0;
+                        Console.WriteLine("\nYou owe ${0} for your {1}.", currBalance, product);
+                        decimal payment = 0;
                         Console.Write("Please insert a coin (5, 10, 25) : ");
                         try
                         {
@@ -95,9 +95,10 @@ namespace InClassExercise1_Mod7
                                 Console.WriteLine("Not a valid input. Please try again.");
                                 continue;
                             }
+                            payment = payment / 100;
                             /* If payment is accepted, add to the total payment. */
                             totalPayment = totalPayment + payment;
-                            Console.WriteLine("\n\t Total Payment: {0}", totalPayment);
+                            Console.WriteLine("\n\t Total Payment: ${0}", totalPayment);
                             currBalance = currBalance - payment;
                         }
                         catch (Exception)
@@ -107,12 +108,14 @@ namespace InClassExercise1_Mod7
                         }
                     }
 
-                    /* Give user a refund if needed (print refund statement.). */
+                    /* Give user a refund if needed (print refund statement.). 
                     if (currBalance < 0)
                     {
                         refund = currBalance * -1;
                         Console.WriteLine("\nYou overpayed for your {0}. Change back : {1} cents", product, refund);
                     }
+                    */
+
                     /* Ask user if they want to purchase more products from machine. */
                     Console.WriteLine("Thank you for your purchase of {0}. Ejoy!\n\nMore products to buy?", product);
                     Console.Write("Enter y or n: ");
@@ -127,13 +130,16 @@ namespace InClassExercise1_Mod7
             /* print the customers final receipt. */
             Console.WriteLine("Customer Receipt.");
             Console.WriteLine("ITEM\t\tQUANTITY\tUNITPRICE\tITEM TOTAL\n");
-            double receiptTotal = 0;
-            foreach (KeyValuePair<string, List<double>> item in receipt)
+            decimal receiptTotal = 0;
+            foreach (KeyValuePair<string, List<decimal>> item in receipt)
             {
-                Console.WriteLine("{0}\t\t   {1}\t\t   {2}\t\t   ${3}", item.Key, item.Value[0], item.Value[1], ((item.Value[0] * item.Value[1])/100));
-                receiptTotal += ((item.Value[0] * item.Value[1]) / 100);
+                Console.WriteLine("{0}\t\t   {1}\t\t   {2}\t\t   ${3}", item.Key, item.Value[0], item.Value[1], ((item.Value[0] * item.Value[1])));
+                receiptTotal += (item.Value[0] * item.Value[1]);
             }
+            refund = totalPayment - receiptTotal;
             Console.WriteLine("\nGRAND TOTAL :\t${0}", receiptTotal);
+            Console.WriteLine("TOTAL AMOUNT PAID : \t${0}", totalPayment);
+            Console.WriteLine("\nCHANGE RETURNED : \t${0}",refund);
 
             Console.ReadKey();
         }
