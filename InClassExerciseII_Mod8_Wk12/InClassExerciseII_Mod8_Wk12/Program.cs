ee0431536d3b98581bbe4cbd30262eff5f11a682
holdenh <holden.halferty@okstate.edu>
2020-04-02 13:31:34 -0500

    Initial Commit.

---
+++ InClassExerciseII_Mod8_Wk12/InClassExerciseII_Mod8_Wk12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/**
+ *      Holden Halferty
+ *      Week 12 Attendance 4/2/2020
+ *      
+ *      Create a small DBMS that makes use of methods to create a Dictionary of students
+ *          and display some information about the students.
+ */
+
+namespace InClassExerciseII_Mod8_Wk12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
