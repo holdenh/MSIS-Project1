dbbd22923de23abd71ae4108905e9f5041594e4e
holdenh <holden.halferty@okstate.edu>
2020-04-07 15:35:45 -0500

    Small exercie with classes. Create an orc class that has some heath, name, and attack. Also, setup getters and setters as well as a displayInfo() method.

---
+++ InClassExerciseMod9_Wk14/InClassExerciseMod9_Wk14/Orc.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InClassExerciseMod9_Wk14
+{
+    class Orc
+    {
+        // variables
+        private string name;
+        private int baseHealth;
+        private int currHealth;
+        private int attackDamage;
+        private Random rand;
+
+        // Constructor
+        public Orc(string name)
+        {
+            rand = new Random();
+            this.name = name;
+            baseHealth = rand.Next(10, 41);     // 10-40
+            currHealth = baseHealth;
+            attackDamage = rand.Next(1, 9);     // 1-8
+        }
+
+        // helper functions for adding and removing health.
+        public void takesDMG(int dmg)
+        {
+            Console.WriteLine("\n{0} takes {1} dmg. . .\n", name, dmg);
+            setCurrHealth(currHealth - dmg);
+        }
+        public void getsHealed(int hp)
+        {
+            setCurrHealth(currHealth + hp);
+        }
+        // set the orc's currentHealth to a specific amount.
+        public void setCurrHealth(int health)
+        {
+            currHealth = health;
+        }
+        public int getCurrHealth()
+        {
+            return currHealth;
+        }
+        // getters and setters for the rest of the variables, will be used if we need access outside of this class.
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public void setBaseHealth(int health)
+        {
+            baseHealth = health;
+        }
+        public int getBaseHealth()
+        {
+            return baseHealth;
+        }
+        public void setAttackDMG(int dmg)
+        {
+            attackDamage = dmg;
+        }
+        public int getAttackDMG()
+        {
+            return attackDamage;
+        }
+        public void displayInfo()
+        {
+            Console.Write("Orc Name : {0}\n\tBase Health : {1}\n\tCurrent Health : {2}\n\tAttack DMG : {3}\n\n", name, baseHealth, currHealth, attackDamage);
+        }
+    }
+}
---
+++ InClassExerciseMod9_Wk14/InClassExerciseMod9_Wk14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InClassExerciseMod9_Wk14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            List<string> orcNames = new List<string> { "Thrall", "Durotan", "Garrosh", "Gul'dan" };
+            List<Orc> battleGroup = new List<Orc>();
+            // create a single orc
+            Orc orc1 = new Orc("Holden");
+
+            orc1.displayInfo();
+            orc1.takesDMG(rand.Next(0,11));    // could do 0 dmg or max, 10 dmg.
+            orc1.displayInfo();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("\nCREATING A GROUP OF ORCS\n");
+
+            //create a new orc for each name in the list, and add to the battlegroup.
+            foreach(string name in orcNames)
+            {
+                Orc newOrc = new Orc(name);
+                battleGroup.Add(newOrc);
+            }
+
+            // for each orc in the battlegroup, display their current info.
+            foreach(Orc orc in battleGroup)
+            {
+                orc.displayInfo();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
